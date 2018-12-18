cbb5026924ebf8650c0b3a3bdb0b5e321e390dc2
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2018-12-18 14:32:56 -0500

    Move a rectangle around the screen

--- DolphEngine.Demo/Game1.cs
+++ DolphEngine.Demo/Game1.cs
@@ -13,6 +13,10 @@ namespace DolphEngine.Demo
 
         private long CurrentTick;
 
+        private Rectangle Rect = new Rectangle(30, 50, 50, 100);
+        private Color RectColor = Color.Red;
+        private Texture2D RectTexture;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -23,39 +27,17 @@ namespace DolphEngine.Demo
         protected override void Initialize()
         {
             Tower.Initialize();
+            this.InitializeControls();
 
-            var keyboard = new StandardKeyboard();
-            var mouse = new StandardMouse();
-            
-            Tower.Keycosystem
-                .AddControlReaction(keyboard, k => k.Escape.IsPressed, k => this.Exit());
+            var data = new Color[this.Rect.Width * this.Rect.Height];
+            this.RectTexture = new Texture2D(this.GraphicsDevice, this.Rect.Width, this.Rect.Height);
 
-            Tower.Keycosystem
-                .AddControlReaction(keyboard, k => k.OemTilde.JustPressed, k => Tower.Debug.Hidden = !Tower.Debug.Hidden)
-                .AddControlReaction(keyboard, k => k.F1.JustPressed, k => Tower.Debug.PrevPage())
-                .AddControlReaction(keyboard, k => k.F2.JustPressed, k => Tower.Debug.NextPage());
+            for (int i = 0; i < data.Length; ++i)
+            {
+                data[i] = new Color(i / 10, i / 10, i / 10);
+            }
 
-            Tower.Keycosystem
-                .AddControlReaction(keyboard, k => k.A.IsPressed, k => this.BackgroundColor = Color.Crimson)
-                .AddControlReaction(keyboard, k => k.Z.IsPressed, k => this.BackgroundColor = Color.DarkOliveGreen)
-                .AddControlReaction(mouse, m => m.PrimaryClick.IsPressed, m => this.BackgroundColor = Color.BurlyWood)
-                .AddControlReaction(mouse, m => m.SecondaryClick.IsPressed, m => this.BackgroundColor = Color.Aquamarine)
-                .AddControlReaction(mouse, m => m.MiddleClick.JustPressed, m => m.LeftHanded = !m.LeftHanded);
-            
-            Tower.Debug.AddLine(1,
-                () => $"CurrentGameTick: {CurrentTick}",
-                DebugLogger.EmptyLine,
-                () => "Control A:",
-                () => $"IsPressed: {keyboard.A.IsPressed}, LastTickPressed: {keyboard.A.LastTickPressed}, LastTickReleased: {keyboard.A.LastTickReleased}",
-                DebugLogger.EmptyLine,
-                () => "Control Z:",
-                () => $"IsPressed: {keyboard.Z.IsPressed}, LastTickPressed: {keyboard.Z.LastTickPressed}, LastTickReleased: {keyboard.Z.LastTickReleased}",
-                DebugLogger.EmptyLine,
-                () => "Mouse:",
-                () => $"PrimaryClick: {mouse.PrimaryClick.IsPressed}, SecondaryClick: {mouse.SecondaryClick.IsPressed}, LeftHanded: {mouse.LeftHanded}",
-                () => $"X: {mouse.Cursor.X}, Y: {mouse.Cursor.Y}");
-
-            Tower.Debug.AddLine(2, "This is page 2!");
+            this.RectTexture.SetData(data);
 
             base.Initialize();
         }
@@ -82,10 +64,53 @@ namespace DolphEngine.Demo
             GraphicsDevice.Clear(this.BackgroundColor);
 
             spriteBatch.Begin(samplerState: SamplerState.PointWrap); // disable anti-aliasing
+            spriteBatch.Draw(this.RectTexture, this.Rect, this.RectColor);
             Tower.Debug.Render(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void InitializeControls()
+        {
+            var keyboard = new StandardKeyboard();
+            var mouse = new StandardMouse();
+
+            Tower.Keycosystem
+                .AddControlReaction(keyboard, k => k.Escape.IsPressed, k => this.Exit());
+
+            Tower.Keycosystem
+                .AddControlReaction(keyboard, k => k.OemTilde.JustPressed, k => Tower.Debug.Hidden = !Tower.Debug.Hidden)
+                .AddControlReaction(keyboard, k => k.F1.JustPressed, k => Tower.Debug.PrevPage())
+                .AddControlReaction(keyboard, k => k.F2.JustPressed, k => Tower.Debug.NextPage());
+
+            Tower.Keycosystem
+                .AddControlReaction(keyboard, k => k.A.IsPressed, k => this.BackgroundColor = Color.Crimson)
+                .AddControlReaction(keyboard, k => k.Z.IsPressed, k => this.BackgroundColor = Color.DarkOliveGreen)
+                .AddControlReaction(mouse, m => m.PrimaryClick.IsPressed, m => this.BackgroundColor = Color.BurlyWood)
+                .AddControlReaction(mouse, m => m.SecondaryClick.IsPressed, m => this.BackgroundColor = Color.Aquamarine)
+                .AddControlReaction(mouse, m => m.MiddleClick.JustPressed, m => m.LeftHanded = !m.LeftHanded);
+
+            Tower.Keycosystem
+                .AddControlReaction(keyboard, k => k.Down.IsPressed, k => this.Rect.Y++)
+                .AddControlReaction(keyboard, k => k.Up.IsPressed, k => this.Rect.Y--)
+                .AddControlReaction(keyboard, k => k.Right.IsPressed, k => this.Rect.X++)
+                .AddControlReaction(keyboard, k => k.Left.IsPressed, k => this.Rect.X--);
+
+            Tower.Debug.AddLine(1,
+                () => $"CurrentGameTick: {CurrentTick}",
+                DebugLogger.EmptyLine,
+                () => "Control A:",
+                () => $"IsPressed: {keyboard.A.IsPressed}, LastTickPressed: {keyboard.A.LastTickPressed}, LastTickReleased: {keyboard.A.LastTickReleased}",
+                DebugLogger.EmptyLine,
+                () => "Control Z:",
+                () => $"IsPressed: {keyboard.Z.IsPressed}, LastTickPressed: {keyboard.Z.LastTickPressed}, LastTickReleased: {keyboard.Z.LastTickReleased}",
+                DebugLogger.EmptyLine,
+                () => "Mouse:",
+                () => $"PrimaryClick: {mouse.PrimaryClick.IsPressed}, SecondaryClick: {mouse.SecondaryClick.IsPressed}, LeftHanded: {mouse.LeftHanded}",
+                () => $"X: {mouse.Cursor.X}, Y: {mouse.Cursor.Y}");
+
+            Tower.Debug.AddLine(2, "This is page 2!");
         }
     }
 }
