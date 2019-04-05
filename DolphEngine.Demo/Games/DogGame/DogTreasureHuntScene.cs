0559eea38a2f8f7e34b474def57ac80f0e3df92d
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2019-04-05 14:10:35 -0400

    DogTreasureHunt stub scene

---
+++ DolphEngine.Demo/Games/DogGame/DogTreasureHuntScene.cs
@@ -0,0 +1,28 @@
+﻿using DolphEngine.Eco;
+using DolphEngine.Input;
+using DolphEngine.Scenery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DolphEngine.Demo.Games.DogGame
+{
+    public class DogTreasureHuntScene : Scene
+    {
+        public DogTreasureHuntScene(Ecosystem ecosystem, Keycosystem keycosystem) 
+            : base(ecosystem, keycosystem)
+        {
+
+        }
+
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Unload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DolphEngine.Demo/Games/DogGame/Entities/DogEntity.cs
@@ -0,0 +1,16 @@
+﻿using DolphEngine.Eco;
+using DolphEngine.Eco.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DolphEngine.Demo.Games.DogGame.Entities
+{
+    public class DogEntity : Entity
+    {
+        public DogEntity() : base("Dog")
+        {
+            this.AddComponent(new SpriteComponent { SpriteSheet = Sprites.DogStuff, Index = 0 });
+        }
+    }
+}
