0f7671c61159f85cc5329006b8fd706b8e860226
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2019-06-02 14:23:04 -0400

    Keys light up on press

--- DolphEngine.Demo/Games/InputTester/KbScene.cs
+++ DolphEngine.Demo/Games/InputTester/KbScene.cs
@@ -4,7 +4,10 @@ using DolphEngine.Eco.Entities;
 using DolphEngine.Eco.Handlers;
 using DolphEngine.Graphics;
 using DolphEngine.Input;
+using DolphEngine.Input.Controllers;
+using DolphEngine.Input.Controls;
 using DolphEngine.Scenery;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -20,26 +23,29 @@ namespace DolphEngine.Demo.Games.InputTester
         private const int HT = 24; // The height of a key on the rendered keyboard
         private const int SPC = 4; // The spacing between keys on the rendered keyboard
 
-        private const int W_KEY = HT;
-        private const int W_WIDE = (int)(HT * 1.25);
-        private const int W_VWIDE = (int)(HT * 1.25);
+        private const int W_SMALL = HT;
+        private const int W_MED = (int)(HT * 1.25);
+        private const int W_XMED = (int)(HT * 1.5);
+        private const int W_LONG = (int)(HT * 1.75);
+        private const int W_XLONG = (int)(HT * 2);
+        private const int W_XXLONG = (int)(HT * 2.5);
         private const int W_SPACE = HT * 8;
 
         private static readonly IReadOnlyDictionary<string, KeyDrawData?> KeySizes = new Dictionary<string, KeyDrawData?>
         {
             #region Data
             { InputKeys.KeyboardNone,           null },
-            { InputKeys.KeyboardBack,           new KeyDrawData(1, 13, W_WIDE) },
-            { InputKeys.KeyboardTab,            new KeyDrawData(2, 0, W_WIDE) },
-            { InputKeys.KeyboardEnter,          new KeyDrawData(3, 12, W_WIDE) },
+            { InputKeys.KeyboardBack,           new KeyDrawData(1, 13, k => k.Back, W_LONG) },
+            { InputKeys.KeyboardTab,            new KeyDrawData(2, 0, k => k.Tab, W_LONG) },
+            { InputKeys.KeyboardEnter,          new KeyDrawData(3, 12, k => k.Enter, W_XLONG) },
             { InputKeys.KeyboardPause,          null },
-            { InputKeys.KeyboardCapsLock,       new KeyDrawData(3, 0, W_WIDE) },
+            { InputKeys.KeyboardCapsLock,       new KeyDrawData(3, 0, k => k.CapsLock, W_XLONG) },
             { InputKeys.KeyboardKana,           null },
             { InputKeys.KeyboardKanji,          null },
-            { InputKeys.KeyboardEscape,         new KeyDrawData(0, 0, W_KEY) },
+            { InputKeys.KeyboardEscape,         new KeyDrawData(0, 0, k => k.Escape) },
             { InputKeys.KeyboardImeConvert,     null },
             { InputKeys.KeyboardImeNoConvert,   null },
-            { InputKeys.KeyboardSpace,          new KeyDrawData(5, 3, W_SPACE) },
+            { InputKeys.KeyboardSpace,          new KeyDrawData(5, 3, k => k.Space, W_SPACE) },
             { InputKeys.KeyboardPageUp,         null },
             { InputKeys.KeyboardPageDown,       null },
             { InputKeys.KeyboardEnd,            null },
@@ -55,44 +61,44 @@ namespace DolphEngine.Demo.Games.InputTester
             { InputKeys.KeyboardInsert,         null },
             { InputKeys.KeyboardDelete,         null },
             { InputKeys.KeyboardHelp,           null },
-            { InputKeys.KeyboardD0,             new KeyDrawData(1, 10, W_KEY) },
-            { InputKeys.KeyboardD1,             new KeyDrawData(1, 1, W_KEY) },
-            { InputKeys.KeyboardD2,             new KeyDrawData(1, 2, W_KEY) },
-            { InputKeys.KeyboardD3,             new KeyDrawData(1, 3, W_KEY) },
-            { InputKeys.KeyboardD4,             new KeyDrawData(1, 4, W_KEY) },
-            { InputKeys.KeyboardD5,             new KeyDrawData(1, 5, W_KEY) },
-            { InputKeys.KeyboardD6,             new KeyDrawData(1, 6, W_KEY) },
-            { InputKeys.KeyboardD7,             new KeyDrawData(1, 7, W_KEY) },
-            { InputKeys.KeyboardD8,             new KeyDrawData(1, 8, W_KEY) },
-            { InputKeys.KeyboardD9,             new KeyDrawData(1, 9, W_KEY) },
-            { InputKeys.KeyboardA,              new KeyDrawData(3, 1, W_KEY) },
-            { InputKeys.KeyboardB,              new KeyDrawData(4, 5, W_KEY) },
-            { InputKeys.KeyboardC,              new KeyDrawData(4, 3, W_KEY) },
-            { InputKeys.KeyboardD,              new KeyDrawData(3, 3, W_KEY) },
-            { InputKeys.KeyboardE,              new KeyDrawData(2, 3, W_KEY) },
-            { InputKeys.KeyboardF,              new KeyDrawData(3, 4, W_KEY) },
-            { InputKeys.KeyboardG,              new KeyDrawData(3, 5, W_KEY) },
-            { InputKeys.KeyboardH,              new KeyDrawData(3, 6, W_KEY) },
-            { InputKeys.KeyboardI,              new KeyDrawData(2, 8, W_KEY) },
-            { InputKeys.KeyboardJ,              new KeyDrawData(3, 7, W_KEY) },
-            { InputKeys.KeyboardK,              new KeyDrawData(3, 8, W_KEY) },
-            { InputKeys.KeyboardL,              new KeyDrawData(3, 9, W_KEY) },
-            { InputKeys.KeyboardM,              new KeyDrawData(4, 7, W_KEY) },
-            { InputKeys.KeyboardN,              new KeyDrawData(4, 6, W_KEY) },
-            { InputKeys.KeyboardO,              new KeyDrawData(2, 9, W_KEY) },
-            { InputKeys.KeyboardP,              new KeyDrawData(2, 10, W_KEY) },
-            { InputKeys.KeyboardQ,              new KeyDrawData(2, 1, W_KEY) },
-            { InputKeys.KeyboardR,              new KeyDrawData(2, 4, W_KEY) },
-            { InputKeys.KeyboardS,              new KeyDrawData(3, 2, W_KEY) },
-            { InputKeys.KeyboardT,              new KeyDrawData(2, 5, W_KEY) },
-            { InputKeys.KeyboardU,              new KeyDrawData(2, 7, W_KEY) },
-            { InputKeys.KeyboardV,              new KeyDrawData(4, 4, W_KEY) },
-            { InputKeys.KeyboardW,              new KeyDrawData(2, 2, W_KEY) },
-            { InputKeys.KeyboardX,              new KeyDrawData(4, 2, W_KEY) },
-            { InputKeys.KeyboardY,              new KeyDrawData(2, 6, W_KEY) },
-            { InputKeys.KeyboardZ,              new KeyDrawData(4, 1, W_KEY) },
-            { InputKeys.KeyboardLeftWindows,    new KeyDrawData(5, 1, W_KEY) },
-            { InputKeys.KeyboardRightWindows,   new KeyDrawData(5, 5, W_KEY) },
+            { InputKeys.KeyboardD0,             new KeyDrawData(1, 10, k => k.D0) },
+            { InputKeys.KeyboardD1,             new KeyDrawData(1, 1, k => k.D1) },
+            { InputKeys.KeyboardD2,             new KeyDrawData(1, 2, k => k.D2) },
+            { InputKeys.KeyboardD3,             new KeyDrawData(1, 3, k => k.D3) },
+            { InputKeys.KeyboardD4,             new KeyDrawData(1, 4, k => k.D4) },
+            { InputKeys.KeyboardD5,             new KeyDrawData(1, 5, k => k.D5) },
+            { InputKeys.KeyboardD6,             new KeyDrawData(1, 6, k => k.D6) },
+            { InputKeys.KeyboardD7,             new KeyDrawData(1, 7, k => k.D7) },
+            { InputKeys.KeyboardD8,             new KeyDrawData(1, 8, k => k.D8) },
+            { InputKeys.KeyboardD9,             new KeyDrawData(1, 9, k => k.D9) },
+            { InputKeys.KeyboardA,              new KeyDrawData(3, 1, k => k.A) },
+            { InputKeys.KeyboardB,              new KeyDrawData(4, 5, k => k.B) },
+            { InputKeys.KeyboardC,              new KeyDrawData(4, 3, k => k.C) },
+            { InputKeys.KeyboardD,              new KeyDrawData(3, 3, k => k.D) },
+            { InputKeys.KeyboardE,              new KeyDrawData(2, 3, k => k.E) },
+            { InputKeys.KeyboardF,              new KeyDrawData(3, 4, k => k.F) },
+            { InputKeys.KeyboardG,              new KeyDrawData(3, 5, k => k.G) },
+            { InputKeys.KeyboardH,              new KeyDrawData(3, 6, k => k.H) },
+            { InputKeys.KeyboardI,              new KeyDrawData(2, 8, k => k.I) },
+            { InputKeys.KeyboardJ,              new KeyDrawData(3, 7, k => k.J) },
+            { InputKeys.KeyboardK,              new KeyDrawData(3, 8, k => k.K) },
+            { InputKeys.KeyboardL,              new KeyDrawData(3, 9, k => k.L) },
+            { InputKeys.KeyboardM,              new KeyDrawData(4, 7, k => k.M) },
+            { InputKeys.KeyboardN,              new KeyDrawData(4, 6, k => k.N) },
+            { InputKeys.KeyboardO,              new KeyDrawData(2, 9, k => k.O) },
+            { InputKeys.KeyboardP,              new KeyDrawData(2, 10, k => k.P) },
+            { InputKeys.KeyboardQ,              new KeyDrawData(2, 1, k => k.Q) },
+            { InputKeys.KeyboardR,              new KeyDrawData(2, 4, k => k.R) },
+            { InputKeys.KeyboardS,              new KeyDrawData(3, 2, k => k.S) },
+            { InputKeys.KeyboardT,              new KeyDrawData(2, 5, k => k.T) },
+            { InputKeys.KeyboardU,              new KeyDrawData(2, 7, k => k.U) },
+            { InputKeys.KeyboardV,              new KeyDrawData(4, 4, k => k.V) },
+            { InputKeys.KeyboardW,              new KeyDrawData(2, 2, k => k.W) },
+            { InputKeys.KeyboardX,              new KeyDrawData(4, 2, k => k.X) },
+            { InputKeys.KeyboardY,              new KeyDrawData(2, 6, k => k.Y) },
+            { InputKeys.KeyboardZ,              new KeyDrawData(4, 1, k => k.Z) },
+            { InputKeys.KeyboardLeftWindows,    new KeyDrawData(5, 1, k => k.LeftWindows) },
+            { InputKeys.KeyboardRightWindows,   new KeyDrawData(5, 5, k => k.RightWindows) },
             { InputKeys.KeyboardApps,           null },
             { InputKeys.KeyboardSleep,          null },
             { InputKeys.KeyboardNumPad0,        null },
@@ -111,18 +117,18 @@ namespace DolphEngine.Demo.Games.InputTester
             { InputKeys.KeyboardSubtract,       null },
             { InputKeys.KeyboardDecimal,        null },
             { InputKeys.KeyboardDivide,         null },
-            { InputKeys.KeyboardF1,             new KeyDrawData(0, 1, W_KEY) },
-            { InputKeys.KeyboardF2,             new KeyDrawData(0, 2, W_KEY) },
-            { InputKeys.KeyboardF3,             new KeyDrawData(0, 3, W_KEY) },
-            { InputKeys.KeyboardF4,             new KeyDrawData(0, 4, W_KEY) },
-            { InputKeys.KeyboardF5,             new KeyDrawData(0, 5, W_KEY) },
-            { InputKeys.KeyboardF6,             new KeyDrawData(0, 6, W_KEY) },
-            { InputKeys.KeyboardF7,             new KeyDrawData(0, 7, W_KEY) },
-            { InputKeys.KeyboardF8,             new KeyDrawData(0, 8, W_KEY) },
-            { InputKeys.KeyboardF9,             new KeyDrawData(0, 9, W_KEY) },
-            { InputKeys.KeyboardF10,            new KeyDrawData(0, 10, W_KEY) },
-            { InputKeys.KeyboardF11,            new KeyDrawData(0, 11, W_KEY) },
-            { InputKeys.KeyboardF12,            new KeyDrawData(0, 12, W_KEY) },
+            { InputKeys.KeyboardF1,             new KeyDrawData(0, 1, k => k.F1) },
+            { InputKeys.KeyboardF2,             new KeyDrawData(0, 2, k => k.F2) },
+            { InputKeys.KeyboardF3,             new KeyDrawData(0, 3, k => k.F3) },
+            { InputKeys.KeyboardF4,             new KeyDrawData(0, 4, k => k.F4) },
+            { InputKeys.KeyboardF5,             new KeyDrawData(0, 5, k => k.F5) },
+            { InputKeys.KeyboardF6,             new KeyDrawData(0, 6, k => k.F6) },
+            { InputKeys.KeyboardF7,             new KeyDrawData(0, 7, k => k.F7) },
+            { InputKeys.KeyboardF8,             new KeyDrawData(0, 8, k => k.F8) },
+            { InputKeys.KeyboardF9,             new KeyDrawData(0, 9, k => k.F9) },
+            { InputKeys.KeyboardF10,            new KeyDrawData(0, 10, k => k.F10) },
+            { InputKeys.KeyboardF11,            new KeyDrawData(0, 11, k => k.F11) },
+            { InputKeys.KeyboardF12,            new KeyDrawData(0, 12, k => k.F12) },
             { InputKeys.KeyboardF13,            null },
             { InputKeys.KeyboardF14,            null },
             { InputKeys.KeyboardF15,            null },
@@ -137,12 +143,12 @@ namespace DolphEngine.Demo.Games.InputTester
             { InputKeys.KeyboardF24,            null },
             { InputKeys.KeyboardNumLock,        null },
             { InputKeys.KeyboardScroll,         null },
-            { InputKeys.KeyboardLeftShift,      new KeyDrawData(4, 0, W_VWIDE) },
-            { InputKeys.KeyboardRightShift,     new KeyDrawData(4, 11, W_VWIDE) },
-            { InputKeys.KeyboardLeftControl,    new KeyDrawData(5, 0, W_WIDE) },
-            { InputKeys.KeyboardRightControl,   new KeyDrawData(5, 7, W_WIDE) },
-            { InputKeys.KeyboardLeftAlt,        new KeyDrawData(5, 2, W_KEY) },
-            { InputKeys.KeyboardRightAlt,       new KeyDrawData(5, 4, W_KEY) },
+            { InputKeys.KeyboardLeftShift,      new KeyDrawData(4, 0, k => k.LeftShift, W_XXLONG) },
+            { InputKeys.KeyboardRightShift,     new KeyDrawData(4, 11, k => k.RightShift, W_XXLONG) },
+            { InputKeys.KeyboardLeftControl,    new KeyDrawData(5, 0, k => k.LeftControl, W_XMED) },
+            { InputKeys.KeyboardRightControl,   new KeyDrawData(5, 7, k => k.RightControl, W_XMED) },
+            { InputKeys.KeyboardLeftAlt,        new KeyDrawData(5, 2, k => k.LeftAlt) },
+            { InputKeys.KeyboardRightAlt,       new KeyDrawData(5, 4, k => k.RightAlt) },
             { InputKeys.KeyboardBrowserBack,    null },
             { InputKeys.KeyboardBrowserForward, null },
             { InputKeys.KeyboardBrowserRefresh, null },
@@ -161,19 +167,19 @@ namespace DolphEngine.Demo.Games.InputTester
             { InputKeys.KeyboardSelectMedia,    null },
             { InputKeys.KeyboardLaunchApplication1, null },
             { InputKeys.KeyboardLaunchApplication2, null },
-            { InputKeys.KeyboardOemSemicolon,   new KeyDrawData(3, 10, W_KEY) },
-            { InputKeys.KeyboardOemPlus,        new KeyDrawData(1, 12, W_KEY) },
-            { InputKeys.KeyboardOemComma,       new KeyDrawData(4, 8, W_KEY) },
-            { InputKeys.KeyboardOemMinus,       new KeyDrawData(1, 11, W_KEY) },
-            { InputKeys.KeyboardOemPeriod,      new KeyDrawData(4, 9, W_KEY) },
-            { InputKeys.KeyboardOemQuestion,    new KeyDrawData(4, 10, W_KEY) },
-            { InputKeys.KeyboardOemTilde,       new KeyDrawData(1, 0, W_KEY) },
+            { InputKeys.KeyboardOemSemicolon,   new KeyDrawData(3, 10, k => k.OemSemicolon) },
+            { InputKeys.KeyboardOemPlus,        new KeyDrawData(1, 12, k => k.OemPlus) },
+            { InputKeys.KeyboardOemComma,       new KeyDrawData(4, 8, k => k.OemComma) },
+            { InputKeys.KeyboardOemMinus,       new KeyDrawData(1, 11, k => k.OemMinus) },
+            { InputKeys.KeyboardOemPeriod,      new KeyDrawData(4, 9, k => k.OemPeriod) },
+            { InputKeys.KeyboardOemQuestion,    new KeyDrawData(4, 10, k => k.OemQuestion) },
+            { InputKeys.KeyboardOemTilde,       new KeyDrawData(1, 0, k => k.OemTilde) },
             { InputKeys.KeyboardChatPadGreen,   null },
             { InputKeys.KeyboardChatPadOrange,  null },
-            { InputKeys.KeyboardOemOpenBrackets, new KeyDrawData(1, 11, W_KEY) },
-            { InputKeys.KeyboardOemPipe,        new KeyDrawData(1, 13, W_WIDE) },
-            { InputKeys.KeyboardOemCloseBrackets, new KeyDrawData(1, 12, W_KEY) },
-            { InputKeys.KeyboardOemQuotes,      new KeyDrawData(3, 11, W_KEY) },
+            { InputKeys.KeyboardOemOpenBrackets, new KeyDrawData(2, 11, k => k.OemOpenBrackets) },
+            { InputKeys.KeyboardOemPipe,        new KeyDrawData(2, 13, k => k.OemPipe, W_MED) },
+            { InputKeys.KeyboardOemCloseBrackets, new KeyDrawData(2, 12, k => k.OemCloseBrackets) },
+            { InputKeys.KeyboardOemQuotes,      new KeyDrawData(3, 11, k => k.OemQuotes) },
             { InputKeys.KeyboardOem8,           null },
             { InputKeys.KeyboardOemBackslash,   null },
             { InputKeys.KeyboardProcessKey,     null },
@@ -213,7 +219,7 @@ namespace DolphEngine.Demo.Games.InputTester
 
         public void Unload()
         {
-            
+            this.Keycosystem.RemoveControlScheme("KeyHighlighter");
         }
 
         public void Update()
@@ -251,6 +257,9 @@ namespace DolphEngine.Demo.Games.InputTester
                 keys.Add(drawnKey);
             }
 
+            var keyboard = this.Keycosystem.GetController<StandardKeyboard>(1);
+            var controlScheme = new ControlScheme();
+
             foreach (var kvp in this.DrawnKeysByRow)
             {
                 var row = kvp.Key;
@@ -267,9 +276,15 @@ namespace DolphEngine.Demo.Games.InputTester
                     keyEntity.AddComponent<DrawComponent>();
                     this.Ecosystem.AddEntity(keyEntity);
 
+                    var keyboardKey = kdd.KeyboardKey(keyboard);
+                    controlScheme.AddControl(() => keyboardKey.JustPressed, () => keyEntity.GetComponent<PolygonComponent>().Color = 0x000000FF);
+                    controlScheme.AddControl(() => keyboardKey.JustReleased, () => keyEntity.GetComponent<PolygonComponent>().Color = 0x00FFFFFF);
+
                     x += kdd.Width + SPC;
                 }
             }
+
+            this.Keycosystem.AddControlScheme("KeyHighlighter", controlScheme);
 
             this.Ecosystem
                 .AddEntity(this.Camera)
@@ -277,9 +292,9 @@ namespace DolphEngine.Demo.Games.InputTester
                 .AddHandler(new DrawHandler(this.Renderer));
 
             // todo: none of this works to move the camera?
-            this.Camera.Lens.Pan = new Vector2d(200, 100);
-            this.Camera.Space.Position.X = 200;
-            this.Camera.Space.Position.Y = 100;
+            // this.Camera.Lens.Pan = new Vector2d(200, 100);
+            this.Camera.Space.Position.X += 200;
+            this.Camera.Space.Position.Y += 100;
         }
 
         private void LoadControls()
@@ -289,21 +304,24 @@ namespace DolphEngine.Demo.Games.InputTester
 
         private struct KeyDrawData
         {
-            public KeyDrawData(int row, int col, int width)
+            public KeyDrawData(int row, int col, Func<StandardKeyboard, SingleButtonControl> keyboardKey, int width = W_SMALL)
             {
                 this.Row = row;
                 this.Col = col;
                 this.Width = width;
                 this.KeyToRead = null;
+                this.KeyboardKey = keyboardKey;
             }
 
             public readonly int Row;
 
             public readonly int Col;
 
-            public readonly int Width;
+            public int Width;
 
             public string KeyToRead;
+
+            public readonly Func<StandardKeyboard, SingleButtonControl> KeyboardKey;
         }
     }
 }
